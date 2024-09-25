2c301fa66e8a22dd564d272c47084790e03e7ce1
yani <yanikif30@gmail.com>
2024-09-25 17:27:46 +0200

    cpp09 ex02 check time

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -6,8 +6,7 @@ PmergeMe::PmergeMe(const PmergeMe& cpy) {*this = cpy;}
 
 PmergeMe&	PmergeMe::operator=(const PmergeMe& other) {
 	if (this != &other) {
-		_vec = other._vec;
-		_paires = other._paires;
+		*this = other;
 	}
 	return *this;
 }
@@ -18,12 +17,14 @@ PmergeMe::PmergeMe(char **av, int ac) {
 	for (int i = 1; i < ac; i++) {
 		parseArgs(av[i]);
 		_vec.push_back(std::atoi(av[i]));
+		_lst.push_back(std::atoi(av[i]));
 	}
 	std::cout << "Before: ";
 	for (size_t	i = 0; i < _vec.size(); i++)
 		std::cout << _vec[i] << " ";
-	std::cout << std::endl;
+	std::cout << std::endl << std::endl;
 	sortVector();
+	sortList();
 }
 
 void	PmergeMe::parseArgs(std::string av) {
@@ -38,26 +39,28 @@ void	PmergeMe::parseArgs(std::string av) {
 
 void	PmergeMe::sortVector() {
 	std::clock_t	start = std::clock();
-	createPairs();
-	sortFusionInsertion();
+	createPairs(_vec, _pairesVec);
+	sortFusionInsertionVec();
 	std::clock_t	end = std::clock();
 	std::cout << "Time to process a range of " << _vec.size() << " elements with std::vector " << double(end - start) / CLOCKS_PER_SEC << " us" << std::endl;
 }
 
-void	PmergeMe::createPairs() {
-	for (size_t	i = 0; i < _vec.size(); i += 2) {
-		if (i + 1 < _vec.size()) {
-			if (_vec[i] > _vec[i + 1])
-				_paires.push_back(std::make_pair(_vec[i + 1], _vec[i]));
-			else
-				_paires.push_back(std::make_pair(_vec[i], _vec[i + 1]));
-		} else {
-			_paires.push_back(std::make_pair(_vec[i], -1));
-		}
+void	PmergeMe::sortFusionInsertionVec() {
+	std::vector<int>	minValueVec;
+	std::vector<int>	maxValueVec;
+
+	for (size_t	i = 0; i < _pairesVec.size(); i++) {
+		minValueVec.push_back(_pairesVec[i].first);
+		if (_pairesVec[i].second != -1)
+			maxValueVec.push_back(_pairesVec[i].second);
 	}
+
+	FordJohnsonSort(maxValueVec, maxValueVec.begin(), maxValueVec.end());
+	std::vector<int> jacobsthal = generateJacobsthalVec(maxValueVec.size());
+	insertValueVec(jacobsthal, minValueVec, maxValueVec);
 }
 
-std::vector<int>	generateJacobsthal(int n) {
+std::vector<int>	PmergeMe::generateJacobsthalVec(int n) {
 	std::vector<int> jacobsthal;
 	jacobsthal.push_back(1);
 	if (n > 1) jacobsthal.push_back(3);
@@ -70,22 +73,7 @@ std::vector<int>	generateJacobsthal(int n) {
 	return jacobsthal;
 }
 
-void	PmergeMe::sortFusionInsertion() {
-	std::vector<int>	first;
-	std::vector<int>	seconds;
-
-	for (size_t	i = 0; i < _paires.size(); i++) {
-		first.push_back(_paires[i].first);
-		if (_paires[i].second != -1)
-			seconds.push_back(_paires[i].second);
-	}
-
-	FordJohnsonSort(seconds, seconds.begin(), seconds.end());
-	std::vector<int> jacobsthal = generateJacobsthal(seconds.size());
-	insertValue(jacobsthal, first, seconds);
-}
-
-void	PmergeMe::insertValue(std::vector<int> jacobsthal, std::vector<int> minValue, std::vector<int> maxValue) {
+void	PmergeMe::insertValueVec(std::vector<int> jacobsthal, std::vector<int> minValue, std::vector<int> &maxValue) {
 	for (size_t	j = 0; j < jacobsthal.size(); j++) {
 		int	index = jacobsthal[j];
 		if (index >= static_cast<int>(minValue.size()))
@@ -97,8 +85,87 @@ void	PmergeMe::insertValue(std::vector<int> jacobsthal, std::vector<int> minValu
 			maxValue.insert(pos, minValue[i]);
 		}
 	}
-	std::cout << "After: ";
+	for (size_t i = 0; i < minValue.size(); ++i) {
+		if (std::find(maxValue.begin(), maxValue.end(), minValue[i]) == maxValue.end()) {
+			std::vector<int>::iterator pos = std::lower_bound(maxValue.begin(), maxValue.end(), minValue[i]);
+			maxValue.insert(pos, minValue[i]);
+		}
+	}
+	std::cout << "After Vector: ";
 	for (size_t	i = 0; i < maxValue.size(); i++)
 		std::cout << maxValue[i] << " ";
+	std::cout << std::endl;
+}
+
+void	PmergeMe::sortList() {
+	std::clock_t	start = std::clock();
+	createPairs(_lst, _pairesLst);
+	sortFusionInsertionLst();
+	std::clock_t	end = std::clock();
+	std::cout << "Time to process a range of " << _vec.size() << " elements with std::list " << double(end - start) / CLOCKS_PER_SEC << " us" << std::endl;
+}
+
+void	PmergeMe::sortFusionInsertionLst() {
+	std::list<int>		minValueLst;
+	std::list<int>		maxValueLst;
+
+	for (std::list<std::pair<int, int> >::iterator it = _pairesLst.begin(); it != _pairesLst.end(); ++it) {
+		minValueLst.push_back(it->first);
+		if (it->second != -1)
+			maxValueLst.push_back(it->second);
+	}
+
+	FordJohnsonSort(maxValueLst, maxValueLst.begin(), maxValueLst.end());
+	std::list<int> jacobsthal = generateJacobsthalLst(maxValueLst.size());
+	insertValueLst(jacobsthal, minValueLst, maxValueLst);
+}
+
+std::list<int>	PmergeMe::generateJacobsthalLst(int n) {
+	std::list<int>	jacobsthal;
+	jacobsthal.push_back(1);
+	if (n > 1) jacobsthal.push_back(3);
+
+	for (int	i = 2; i < n;++i) {
+		std::list<int>::iterator	it = jacobsthal.end();
+		int next = *(--it) + 2 * *(--it);
+		if (next >= n) break;
+		jacobsthal.push_back(next);
+	}
+	return jacobsthal;
+}
+
+void	PmergeMe::insertValueLst(std::list<int> jacobsthal, std::list<int> minValue, std::list<int> &maxValue) {
+	std::list<int>::iterator it = jacobsthal.begin();
+	for (size_t j = 0; j < jacobsthal.size(); ++j, ++it) {
+		int index = *it;
+		if (index >= static_cast<int>(minValue.size()))
+			index = minValue.size() - 1;
+		int prev_index = -1;
+		if (j > 0) {
+			std::list<int>::iterator prevIt = it;
+			--prevIt;
+			prev_index = *prevIt;
+		}
+
+		std::list<int>::iterator minIt = minValue.begin();
+		std::advance(minIt, index);
+
+		for (int i = index; i > prev_index && i >= 0; --i) {
+			std::list<int>::iterator pos = std::lower_bound(maxValue.begin(), maxValue.end(), *minIt);
+			if (std::find(maxValue.begin(), maxValue.end(), *minIt) == maxValue.end())
+				maxValue.insert(pos, *minIt);
+			if (i > 0) --minIt;
+		}
+	}
+
+	for (std::list<int>::iterator	minIt = minValue.begin(); minIt != minValue.end(); ++minIt) {
+		if (std::find(maxValue.begin(), maxValue.end(), *minIt) == maxValue.end()) {
+			std::list<int>::iterator	pos = std::lower_bound(maxValue.begin(), maxValue.end(), *minIt);
+			maxValue.insert(pos, *minIt);
+		}
+	}
+	std::cout << std::endl << "After List: ";
+	for (std::list<int>::iterator	it = maxValue.begin(); it != maxValue.end(); ++it)
+		std::cout << *it << " ";
 	std::cout << std::endl;
 }
--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -18,6 +18,7 @@
 #include <climits>
 #include <ctime>
 #include <vector>
+#include <list>
 
 class PmergeMe {
 public:
@@ -25,15 +26,21 @@ public:
 	~PmergeMe();
 
 	void	parseArgs(std::string av);
-	void	createPairs();
-	void	sortFusionInsertion();
-	//void	mergecontainer(std::vector<int>& vec, int left, int mid, int right);
-	void	insertValue(std::vector<int> jacobsthal, std::vector<int> minValue, std::vector<int> maxValue);
+	void	sortFusionInsertionVec();
+	void	sortFusionInsertionLst();
+	void	insertValueVec(std::vector<int> jacobsthal, std::vector<int> minValue, std::vector<int> &maxValue);
+	void	insertValueLst(std::list<int> jacobsthal, std::list<int> minValue, std::list<int> &maxValue);
+	std::vector<int>	generateJacobsthalVec(int n);
+	std::list<int>		generateJacobsthalLst(int n);
+
 	void	sortVector();
+	void	sortList();
 
 private:
 	std::vector<int>	_vec;
-	std::vector<std::pair<int, int> >	_paires;
+	std::vector<std::pair<int, int> >	_pairesVec;
+	std::list<int>	_lst;
+	std::list<std::pair<int, int> >	_pairesLst;
 
 	PmergeMe();
 	PmergeMe(const PmergeMe& cpy);
@@ -43,9 +50,30 @@ private:
 template<typename T, typename Iterator>
 void	FordJohnsonSort(T &container, Iterator left, Iterator right) {
 	if (std::distance(left, right) > 1) {
-		Iterator mid = left + std::distance(left, right) / 2;
+		Iterator	mid = left;
+		std::advance(mid, std::distance(left, right) / 2);
 		FordJohnsonSort(container, left, mid);
 		FordJohnsonSort(container, mid, right);
 		std::inplace_merge(left, mid, right);
+	}
+}
+
+template<typename T, typename P>
+void	createPairs(T &container, P &paires) {
+	typename T::iterator	it = container.begin();
+	while (it != container.end()) {
+		typename T::iterator	nextIt = it;
+		nextIt++;
+		if (nextIt != container.end()) {
+			if (*it > *nextIt)
+				paires.push_back(std::make_pair(*nextIt, *it));
+			else
+				paires.push_back(std::make_pair(*it, *nextIt));
+			++it;
+			++it;
+		} else {
+			paires.push_back(std::make_pair(*it, -1));
+			break;
+		}
 	}
 }
